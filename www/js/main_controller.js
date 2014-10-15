5f8f764c15755b925e49b12bb0ac0cf7d8031d9a
Zoe Ames <zoeames@gmail.com>
2014-10-15 13:46:29 -0500

    trig functions added

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -49,6 +49,53 @@
       }
     };
 
+    $scope.square = function(){
+      var x   = $scope.display * 1,
+      ans = x*x;
+      $scope.display = ans;
+    };
+
+    $scope.root = function(){
+      var x   = $scope.display * 1,
+      ans = Math.sqrt(x);
+      $scope.display = ans;
+    };
+
+    $scope.cosfunc = function(){
+      var x  = $scope.display * 1,
+      ans = Math.cos(x);
+      $scope.display = ans;
+    };
+
+    $scope.sinfunc = function(){
+      var x  = $scope.display * 1,
+      ans = Math.sin(x);
+      $scope.display = ans;
+    };
+
+    $scope.tanfunc = function(){
+      var x  = $scope.display * 1,
+      ans = Math.tan(x);
+      $scope.display = ans;
+    };
+
+    $scope.acosfunc = function(){
+      var x  = $scope.display * 1,
+      ans = Math.acos(x);
+      $scope.display = ans;
+    };
+
+    $scope.asinfunc = function(){
+      var x  = $scope.display * 1,
+      ans = Math.asin(x);
+      $scope.display = ans;
+    };
+
+    $scope.atanfunc = function(){
+      var x  = $scope.display * 1,
+      ans = Math.atan(x);
+      $scope.display = ans;
+    };
 
   }]);
 })();
